81e499962cd5781327130a5f36f4f66b1defce5d
jguz17 <johnathanguzman17@gmail.com>
2021-03-17 19:18:44 -0500

    Added date formatting

--- src/components/invoices/InvoiceItem.js
+++ src/components/invoices/InvoiceItem.js
@@ -14,17 +14,17 @@ const InvoiceItem = (props) => {
         bgColor = "#FFF9F0"
     } 
 
-    // var  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    // var d = new Date();
-    // var monthName=months[d.getMonth()]; // "July" (or current month)
-    // let shortMonth = d.toLocaleString('en-us', { month: 'short' }); /* Jun */
+    var  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var d = new Date();
+    let shortMonth = d.toLocaleString('en-us', { month: 'short' }); /* Jun */
+    let date = d.getDate()
+    let year = d.getFullYear()
 
-    // console.log(shortMonth)
-
+    const shortDate = `${date} ${shortMonth} ${year}`
     return (
         <div style={{ display: "flex", padding: "1.5rem", justifyContent: "space-between", backgroundColor: "#fff", marginBottom: ".5rem", boxShadow: "0px 3px 5px 0px rgba(0,0,0,0.05)", borderRadius: "5px", alignItems: "center" }}>
             <div style={{ width: "150px" }}><h3><span style={{ color: "#888EB0" }}>#</span>{props.invoiceItem.id}</h3></div>
-            <div style={{ width: "150px", color: "#888EB0" }}><p>Due {props.invoiceItem.paymentDue}</p></div>
+            <div style={{ width: "150px", color: "#888EB0" }}><p>Due {shortDate}</p></div>
             <div style={{ width: "150px", color: "#888EB0" }}><p>{props.invoiceItem.clientName}</p></div>
             <div style={{ width: "150px", textAlign: "center" }}><h3>${props.invoiceItem.total}</h3></div>
             <div style={{ width: "125px", height: "50px", backgroundColor: `${bgColor}`, color: `${color}`, display: "flex", alignItems: "center", justifyContent: "center", borderRadius: "5px" }}><FiberManualRecordIcon fontSize="small" style={{ marginRight: ".25rem" }}/><p>{props.invoiceItem.status.charAt(0).toUpperCase() + props.invoiceItem.status.slice(1)}</p></div>
