27e5b59909fe5ede3ce12f71c64be42549f35085
Johnathan Guzman <johnathanguzman17@gmail.com>
2021-03-19 00:22:35 -0500

    Connected to billy invoice api

--- src/components/invoices/Invoice.js
+++ src/components/invoices/Invoice.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InvoiceItem from "./InvoiceItem"
 import plusLogo from "../layout/plus-circle.svg"
 import Dropdown from "../layout/Dropdown"
@@ -7,21 +7,29 @@ const InvoiceData = require("../../data.json")
 const uniqid = require('uniqid');
 
 const Invoice = () => {
+
+    const [invoices, setInvoices] = useState(0);
+
+    fetch("https://billy-invoice-api.herokuapp.com/invoices")
+    .then(res => res.json())
+    .then(data => setInvoices(data))
+
     return (
         <div id="invoice-container" >
             <div id="invoice-wrapper" >
                 <header style={{ display: "flex", padding: "5px", justifyContent: "space-between", marginBottom: "2.5rem"}}>
                     <div>
                         <h2 style={{ width: "150px", fontSize: "2rem"}}>Invoices</h2>
-                        <p>There are {InvoiceData.length} total invoices</p>
+                        <p>There are {invoices.length} total invoices</p>
                     </div>
                     <div style={{ display: "flex", alignItems: "center" }}>
                         <Dropdown/>
                         <Button style={{ backgroundColor: "#7C5DFA", border: "none", padding: ".5rem 1rem", color: "white", borderRadius: "2rem", display: "flex", alignItems: "center", outline: "none" }}><img src={plusLogo} alt="Plus sign logo" style={{ marginRight: ".5rem" }}/>New Invoice</Button>
                     </div>
                 </header>
-                {InvoiceData.map(invoiceItem => {
-                    return <InvoiceItem key={uniqid()} invoiceItem={invoiceItem}/>
+                
+                {invoices && invoices.map(invoiceItem => {
+                    return <InvoiceItem key={invoiceItem.id} invoiceItem={invoiceItem}/>
                 })}
             </div>
         </div>
--- src/components/invoices/InvoiceItem.js
+++ src/components/invoices/InvoiceItem.js
@@ -15,17 +15,18 @@ const InvoiceItem = (props) => {
     } 
 
     var  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var d = new Date();
+    var d = new Date(props.invoiceItem.date);
     let shortMonth = d.toLocaleString('en-us', { month: 'short' }); /* Jun */
     let date = d.getDate()
     let year = d.getFullYear()
 
     const shortDate = `${date} ${shortMonth} ${year}`
+
     return (
         <div id="invoice-item-container">
             <div><h3><span style={{ color: "#888EB0" }}>#</span>{props.invoiceItem.id}</h3></div>
             <div style={{ color: "#888EB0"}}><p>Due {shortDate}</p></div>
-            <div className="text-align-center" style={{ color: "#888EB0" }}><p>{props.invoiceItem.clientName}</p></div>
+            <div className="text-align-center" style={{ color: "#888EB0" }}><p>{props.invoiceItem.client_name}</p></div>
             <div className="text-align-center"><h3>${props.invoiceItem.total}</h3></div>
             <div className="" style={{ width: "150px", height: "50px", backgroundColor: `${bgColor}`, color: `${color}`, display: "flex", alignItems: "center", justifyContent: "center", borderRadius: "5px" }}><FiberManualRecordIcon fontSize="small" style={{ marginRight: ".25rem" }}/><p>{props.invoiceItem.status.charAt(0).toUpperCase() + props.invoiceItem.status.slice(1)}</p></div>
             <KeyboardArrowRightIcon id="right-arrow" style={{ color: "#7C5DFA" }}/>
