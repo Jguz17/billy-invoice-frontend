3b305f32589fb6363cb8ddc952d41cdb9ef7de43
Johnathan Guzman <johnathanguzman17@gmail.com>
2021-03-19 16:41:27 -0500

    Finished transitioning from inline css to external

--- src/components/layout/MobileNav.js
+++ src/components/layout/MobileNav.js
@@ -6,9 +6,9 @@ const MobileNav = () => {
     return (
         <div id="mobile-nav">
             <img id="mobile-nav-logo" src={logo} alt="logo"/>
-            <div >
-                <img style={{width: "30px"}} src={moonLogo} style={{ marginRight: "1rem" }}/>
-                <div style={{ width: "1px", border: "1px solid #7E88C3", height: "75px", margin: "0 1rem" }}></div>
+            <div>
+                <img id="mobile-nav-moon" src={moonLogo} alt="Moon logo"/>
+                <div className="vertical-line"></div>
                 <div id="mobile-nav-avatar"></div>
             </div>            
         </div>
