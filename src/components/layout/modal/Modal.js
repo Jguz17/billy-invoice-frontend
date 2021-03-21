fbdc2562c0421949931c320b543b5092c114f513
Johnathan Guzman <johnathanguzman17@gmail.com>
2021-03-20 19:31:49 -0500

    Finished modal styles and rendering. Need to add functionality now

--- src/components/layout/modal/Modal.js
+++ src/components/layout/modal/Modal.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import Button from '@material-ui/core/Button'
 import HighlightOffIcon from '@material-ui/icons/HighlightOff'
 import ModalContext from "../../../context/modal/modalContext"
+import TextField from '@material-ui/core/TextField';
 
 const Modal = () => {
 
@@ -11,7 +12,24 @@ const Modal = () => {
 
     return (
         <div id="modal-main">
-            <Button onClick={() => turnOffModal()} id="button"><HighlightOffIcon className="xs-margin-right"/> Cancel</Button>
+            <TextField className="fluid" id="outlined-search" label="Client Name" type="search" variant="outlined" />
+            <TextField className="fluid" id="outlined-search" label="Street Address" type="search" variant="outlined" />
+            <div>
+                <TextField className="fluid-s"
+                    id="date"
+                    label="Invoice date"
+                    type="date"
+                    InputLabelProps={{
+                    shrink: true,
+                    }}
+                />
+                <TextField className="fluid-s" id="outlined-search" label="Enter total" type="search" variant="outlined" />
+                
+            </div>
+            <div id="modal-button-container">
+                <Button style={{ backgroundColor: "#F9FAFE", color: "#7E88C3" }} className="fluid-s" onClick={() => turnOffModal()} id="button"><HighlightOffIcon className="xs-margin-right"/> Cancel</Button>
+                <Button className="fluid-s" onClick={() => turnOffModal()} id="button"><HighlightOffIcon className="xs-margin-right"/> Cancel</Button>
+            </div>
         </div>
     )
 }
